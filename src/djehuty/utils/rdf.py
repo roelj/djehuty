ea36e68a544e053a18b260ef205b4caa951550a6
Roel Janssen <rrejanssen@gmail.com>
2025-11-24 15:11:56 +0100

    utils: Remove unused procedures and variables.
    * src/djehuty/utils/convenience.py: Remove ‘opendap_sizes_to_bytes’.
    * src/djehuty/utils/rdf.py: Remove “ROW”.

--- src/djehuty/utils/rdf.py
+++ src/djehuty/utils/rdf.py
@@ -7,7 +7,6 @@ import uuid
 from rdflib import Literal, Namespace, URIRef, XSD
 
 BLANK = Namespace("blank:")
-ROW   = Namespace("djehuty:")
 DJHT  = Namespace("https://ontologies.data.4tu.nl/djehuty/0.0.1/")
 
 ## Pre-compiled patterns for determining the SPARQL query type.
