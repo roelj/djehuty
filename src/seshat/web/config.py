defdb8c2576df772b81a7492348ef4afae6abd15
Malaika Newman <symmetricbutterfly@proton.me>
2026-04-25 12:47:23 +0200

    Update Git URL in more places.
    * debian/control: Update Git URL.
    * docker/Dockerfile: Update Git URL.
    * src/seshat/web/config.py: Update Git URL.

--- src/seshat/web/config.py
+++ src/seshat/web/config.py
@@ -73,7 +73,7 @@ class RuntimeConfiguration:  # pylint: disable=too-few-public-methods
         self.handle_index                = None
         self.small_footer                = (
             '<div id="footer-wrapper2"><p>This repository is powered by '
-            '<a href="https://codeberg.org/roelj/djehuty">seshat</a>.'
+            '<a href="https://codeberg.org/seshat/seshat">seshat</a>.'
             '</p></div>'
         )
         self.large_footer                = self.small_footer
